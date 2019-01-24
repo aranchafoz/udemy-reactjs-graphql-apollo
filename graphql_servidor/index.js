e7120285a937a923860bd00df45984867ca11133
Arancha Ferrero <aranchunfli11foz@hotmail.com>
2019-01-24 21:21:35 +0100

    Hello world from GraphQL

--- graphql_servidor/index.js
+++ graphql_servidor/index.js
@@ -1,9 +1,24 @@
 import express from 'express'
+// graphql
+import graphqlHTTTP from 'express-graphql'
+import schema from './schema'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Todo listo')
+  res.send('Ready')
 })
 
-app.listen(8000, () => console.log('El servidor está funcionando'))
+// resolver
+const root = {hello: () => "Hello world from GraphQL"}
+
+app.use('/graphql', graphqlHTTTP({
+  // schema which wil be use it
+  schema,
+  // resolver is sent as rootValue
+  rootValue: root,
+  // use graphiql
+  graphiql : true
+}))
+
+app.listen(8000, () => console.log('Server is running'))
